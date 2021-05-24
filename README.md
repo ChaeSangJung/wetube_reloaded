87c8653a68f4e3c3aba234e4ed9d02c2a8493de1
Chae Sang, Jung <shtingstar@hanmail.net>
2021-05-24 22:40:09 +0900

    #3.5 Middlewares part One

--- README.md
+++ README.md
@@ -26,4 +26,16 @@
         - res = 정보를 응답! ( res.send )
 - #3.4 Recap
 - #3.5 Middlewares part One
+    - 라우팅 후, 콜백함수 사이에서 동작할 함수. 즉, 사이에 있는 함수.
+    - 주의 : next()가 꼭 필요하다 !! 안그럼 다음에 실행할 콜백함수가 실행하지 못할거야!!
+    - 각개적용 = 라우팅 - 콜백 사이에 직접 위치해줌.
+    - 모두적용 = app.use () 로 적용해주고, 해당 코드 아래에 있는 모든 코드에 적용됨
+
+    - Middleware : 유저와 마지막 응답 사이에 존재하는 것
+    - 1. 처리가 끝날 때까지 연결되어 있는 것
+        - 시작 : browser에서 접속하려 할 때 그때가 "시작"
+        - index 파일 실행
+            - route가 존재하는지 살펴봄(/)
+            - route 찾음 => handleHome 실행
+            - handleHome 응답을 전송
 - #3.6 Middlewares part Two
--- src/server.js
+++ src/server.js
@@ -4,14 +4,15 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = (req, res) => {    
-    return res.send("I still love you.");    
+const gossipMiddleware = (req, res, next) => {
+    console.log(`Someone is going to: ${req.url}`);
+    next();
 };
-const handleLogin = (req, res) => {
-    return res.send("Login here.");
+const handleHome = (req, res) => {
+    return res.send("I love middlewares");
 };
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+
+app.get("/", gossipMiddleware, handleHome);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
