336c78f4f4daafb769c834b5d92e0201327c62fc
Chae Sang, Jung <shtingstar@hanmail.net>
2021-05-30 22:39:19 +0900

    #6.1 Array Database part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,7 @@ let videos = [
         rating: 5,
         comments: 2,
         createdAt: "2 minutes ago",
-        views: 59,
+        views: 1,
         id: 1,
     },
     {
@@ -28,11 +28,11 @@ let videos = [
 export const trending = (req, res) => {    
     return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => {
+export const watch = (req, res) => {
     const { id } = req.params;
     // const id = req.params.id
-    const video = videos[id - 1];
-    return res.render("watch", { pageTitle: `Watching ${video.title}` });
+    const video = videos[id - 1];    
+    return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
