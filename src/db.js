5fff3342a77456710f278a544095a44c8905b3c6
Chae Sang, Jung <shtingstar@hanmail.net>
2021-06-08 22:32:41 +0900

    #6.27 Search part Two

--- src/db.js
+++ src/db.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 
 const db = mongoose.connection;
@@ -11,4 +12,3 @@ const handleOpen = () => console.log("✅ Connected to DB");
 const handleError = (error) => console.log("❌ DB Error", error);
 
 db.on("error", handleError);
-db.once("open", handleOpen);
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,17 +1,18 @@
 import express from "express";
 import {
-    watch,
-    getUpload,
-    getEdit,
-    postEdit,
-    postUpload,
-
+  watch,
+  getUpload,
+  getEdit,
+  postEdit,
+  postUpload,
+  deleteVideo,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.route("/:id(\\d+)").get(watch);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
